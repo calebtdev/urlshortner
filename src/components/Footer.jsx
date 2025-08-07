e282dcb567d3b0eb5f38452e26a6fe72e2076c82
Caleb Adebayo <tolorunlekecaleb@gmail.com>
2025-08-06 19:13:43 -0700

    update on the urlshortnerproject

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,28 +16,47 @@ const Footer = () => {
           <div className="flex flex-col gap-5">
             <span className="text-white">Features</span>
             <ul className="text-gray-400 flex flex-col gap-2">
-              <li className="hover:text-cyan-500 cursor-pointer">
+              <li className="hover:text-cyan-500 transition-colors duration-300 ease-out cursor-pointer">
                 Link Shortening
               </li>
-              <li>Branded Links</li>
-              <li>Analytics</li>
+              <li className="hover:text-cyan-500 transition-colors duration-300 ease-out cursor-pointer">
+                Branded Links
+              </li>
+              <li className="hover:text-cyan-500 transition-colors duration-300 ease-out cursor-pointer">
+                Analytics
+              </li>
             </ul>
           </div>
           <div className="flex flex-col gap-5">
             <span className="text-white">Resources</span>
             <ul className="text-gray-400 flex flex-col gap-2">
-              <li>Blog</li>
-              <li>Developers</li>
-              <li>Support</li>
+              <li className="hover:text-cyan-500 transition-colors duration-300 ease-out cursor-pointer">
+                {" "}
+                Blog
+              </li>
+              <li className="hover:text-cyan-500 transition-colors duration-300 ease-out cursor-pointer">
+                Developers
+              </li>
+              <li className="hover:text-cyan-500 transition-colors duration-300 ease-out cursor-pointer">
+                Support
+              </li>
             </ul>
           </div>
           <div className="flex flex-col gap-5">
             <span className="text-white">Company</span>
             <ul className="text-gray-400 flex flex-col gap-2">
-              <li>About</li>
-              <li>Our Team</li>
-              <li>Careers</li>
-              <li>Contact</li>
+              <li className="hover:text-cyan-500 transition-colors duration-300 ease-out cursor-pointer">
+                About
+              </li>
+              <li className="hover:text-cyan-500 transition-colors duration-300 ease-out cursor-pointer">
+                Our Team
+              </li>
+              <li className="hover:text-cyan-500 transition-colors duration-300 ease-out cursor-pointer">
+                Careers
+              </li>
+              <li className="hover:text-cyan-500 transition-colors duration-300 ease-out cursor-pointer">
+                Contact
+              </li>
             </ul>
           </div>
           <div className="flex flex-row gap-5">
